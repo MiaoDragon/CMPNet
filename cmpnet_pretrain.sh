461b2c2896e9c297a0174527aa380a83b2aed551
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 11:55:58 -0800

    chmod

---
+++ cmpnet_pretrain.sh
@@ -0,0 +1,6 @@
+python3 cmpnet_train.py --model_path ../CMPnet_res/s2d/ \
+--no_env 4 --no_motion_paths 200 --grad_step 1 --learning_rate 0.01 \
+--num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
+--n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
+--start_epoch 0 --data_path ../data/simple/ --world_size 20 --env_type s2d \
+--memory_type res
