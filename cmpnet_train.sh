7ee823e9a609674ff378aaf78c10c4070e94e9d9
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 13:50:36 -0800

    change device

--- cmpnet_train.sh
+++ cmpnet_train.sh
@@ -7,6 +7,6 @@
 python3 cmpnet_train.py --model_path ../CMPnet_res/s2d/ \
 --no_env 10 --no_motion_paths 500 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
---n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
+--n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path ../data/simple/ --world_size 20 --env_type s2d \
 --memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32
