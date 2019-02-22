37e2dcf1b5ca666c904fe5d7350c83feb5fcfe9b
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 13:44:57 -0800

    can't call module

--- cmpnet_train.py
+++ cmpnet_train.py
@@ -52,20 +52,20 @@ def main(args):
         load_dataset = data_loader_2d.load_dataset
         normalize = utility_s2d.normalize
         unnormalize = utility_s2d.unnormalize
-        mpNet.encoder = CAE()
-        mpNet.mlp = model(args.mlp_input_size, args.output_size)
+        mpNet.encoder = CAE.Encoder()
+        mpNet.mlp = model.MLP(args.mlp_input_size, args.output_size)
     elif args.env_type == 'c2d':
         load_dataset = data_loader_2d.load_dataset
         normalize = utility_c2d.normalize
         unnormalize = utility_c2d.unnormalize
-        mpNet.encoder = CAE()
-        mpNet.mlp = model_c2d(args.mlp_input_size, args.output_size)
+        mpNet.encoder = CAE.Encoder()
+        mpNet.mlp = model_c2d.MLP(args.mlp_input_size, args.output_size)
     elif args.env_type == 'r3d':
         load_dataset = data_loader_r3d.load_dataset
         normalize = utility_r3d.normalize
         unnormalize = utility_r3d.unnormalize
-        mpNet.encoder = CAE_r3d()
-        mpNet.mlp = model(args.mlp_input_size, args.output_size)
+        mpNet.encoder = CAE_r3d.Encoder()
+        mpNet.mlp = model.MLP(args.mlp_input_size, args.output_size)
 
     # load previously trained model if start epoch > 0
     model_path='cmpnet_epoch_%d.pkl' %(args.start_epoch)
