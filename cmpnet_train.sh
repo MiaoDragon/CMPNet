fe1b0692ec121f45a0157f247ae9666a96d91edc
MiaoDragon <innocentmyl@gmail.com>
2019-02-24 23:51:45 -0800

    added output size

--- cmpnet_train.sh
+++ cmpnet_train.sh
@@ -9,4 +9,4 @@ python3 cmpnet_train.py --model_path ../CMPnet_res/s2d/ \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path ../data/simple/ --world_size 20 --env_type s2d \
---memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32
+--memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
