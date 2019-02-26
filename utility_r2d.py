b0ac5645e1f7da3c33ef1692f0a1cac5bdf4f7e9
MiaoDragon <innocentmyl@gmail.com>
2019-02-26 15:58:08 -0800

    deb

--- utility_r2d.py
+++ utility_r2d.py
@@ -15,12 +15,13 @@ def normalize(x, bound):
     return x
 def unnormalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
+    # x only one dim
     bound = torch.tensor(bound)
-    if len(x[0]) != len(bound):
+    if len(x) != len(bound):
         # then the proceding is obstacle
-        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] * bound[0]
-        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] * bound
-        x[:,-len(bound):] = x[:,-len(bound):] * bound
+        x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
+        x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
+        x[-len(bound):] = x[-len(bound):] * bound
     else:
         x = x * bound
     return x
