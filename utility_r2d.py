4b942fffe76591387066bc1b9480b5ab2a43a2c5
MiaoDragon <innocentmyl@gmail.com>
2019-02-26 15:34:41 -0800

    rigid body utility debug

--- utility_r2d.py
+++ utility_r2d.py
@@ -4,6 +4,23 @@ import copy
 import numpy as np
 def normalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
-    return x / torch.tensor(bound)
+    bound = torch.tensor(bound)
+    if len(x[0]) != len(bound):
+        # then the proceding is obstacle
+        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
+        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
+        x[:-len(bound):] = x[:,-len(bound)] / bound
+    else:
+        x = x / bound
+    return x
 def unnormalize(x, bound):
-    return x * torch.tensor(bound)
+    # normalize to -1 ~ 1  (bound can be a tensor)
+    bound = torch.tensor(bound)
+    if len(x[0]) != len(bound):
+        # then the proceding is obstacle
+        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] * bound[0]
+        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] * bound
+        x[:-len(bound):] = x[:,-len(bound)] * bound
+    else:
+        x = x * bound
+    return x
