ca308ce9f5e5002a67a7a5aef798de13a8353691
MiaoDragon <innocentmyl@gmail.com>
2019-02-27 10:05:20 -0800

    added 3d cases

--- cmpnet_test_r2d.sh
+++ cmpnet_test_r2d.sh
@@ -1,7 +1,7 @@
 python3 cmpnet_test.py --model_path ../CMPnet_res/r2d/ \
 --grad_step 1 --learning_rate 0.01 \
 --memory_strength 0.5 --n_memories 10000 \
---n_tasks 1 --device 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ \
+--n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ \
 --start_epoch 1 --memory_type res --env_type r2d --world_size 20 \
 --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3 \
 --seen_N 100 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
