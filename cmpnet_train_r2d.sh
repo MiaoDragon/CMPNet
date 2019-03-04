c9e74d5ffde17bad034ba9f36ca5bb65c8a9ffb8
MiaoDragon <innocentmyl@gmail.com>
2019-03-04 10:31:04 -0800

    change device number

--- cmpnet_train_r2d.sh
+++ cmpnet_train_r2d.sh
@@ -3,6 +3,6 @@
 python3 cmpnet_train.py --model_path ../CMPnet_res/r2d/ \
 --no_env 100 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
---n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
+--n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --world_size 20 --env_type r2d \
 --memory_type res --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3
