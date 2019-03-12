000b513fc603fbb558992972da949ccaab7683d4
MiaoDragon <innocentmyl@gmail.com>
2019-03-11 23:08:30 -0700

    don't normalize obstacles, but normalize state

--- utility_r2d.py
+++ utility_r2d.py
@@ -3,32 +3,28 @@ from torch.autograd import Variable
 import copy
 import numpy as np
 def normalize(x, bound):
-    """
     # normalize to -1 ~ 1  (bound can be a tensor)
     bound = torch.tensor(bound)
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
-        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
+        # don't normalize obstacles
+        #x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
         x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
         x[:,-len(bound):] = x[:,-len(bound):] / bound
     else:
         x = x / bound
     return x
-    """
-    return x
 
 def unnormalize(x, bound):
-    """
     # normalize to -1 ~ 1  (bound can be a tensor)
     # x only one dim
     bound = torch.tensor(bound)
     if len(x) != len(bound):
         # then the proceding is obstacle
-        x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
+        # don't normalize obstacles
+        #x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
         x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
         x[-len(bound):] = x[-len(bound):] * bound
     else:
         x = x * bound
-    return x
-    """
     return x
