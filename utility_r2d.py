dcb1c2b22fc7ff1283337dfbf22eed2eba631c48
MiaoDragon <innocentmyl@gmail.com>
2019-03-14 01:51:55 -0700

    bug

--- utility_r2d.py
+++ utility_r2d.py
@@ -22,7 +22,7 @@ def unnormalize(x, bound):
     if len(x) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
+        #x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
         x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
         x[-len(bound):] = x[-len(bound):] * bound
     else:
