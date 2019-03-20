bf74c9908aec6b4478e47a284a280f2f147e5d27
MiaoDragon <innocentmyl@gmail.com>
2019-03-19 17:26:12 -0700

    disable normalization and try

--- utility_r2d.py
+++ utility_r2d.py
@@ -4,6 +4,7 @@ import copy
 import numpy as np
 def normalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
+    return x
     bound = torch.tensor(bound)
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
@@ -18,6 +19,7 @@ def normalize(x, bound):
 def unnormalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
     # x only one dim
+    return x
     bound = torch.tensor(bound)
     if len(x) != len(bound):
         # then the proceding is obstacle
