db36549fa61a312a42de5685347a4126967a5b56
MiaoDragon <innocentmyl@gmail.com>
2019-04-04 14:41:03 -0700

    normalize

--- utility_r2d.py
+++ utility_r2d.py
@@ -4,12 +4,12 @@ import copy
 import numpy as np
 def normalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
-    return x
+    #return x
     bound = torch.tensor(bound)
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        #x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0] * 20.
+        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
         #print('before normalize...')
         #print(x[:,-2*len(bound):])
         x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
@@ -23,12 +23,12 @@ def normalize(x, bound):
 def unnormalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
     # x only one dim
-    return x
+    #return x
     bound = torch.tensor(bound)
     if len(x) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
-        #x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0] / 20.
+        x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
         x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
         x[-len(bound):] = x[-len(bound):] * bound
     else:
