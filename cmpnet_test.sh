a22366537aaf03372213b065ecfb1a2f169c0576
MiaoDragon <innocentmyl@gmail.com>
2019-04-04 14:39:04 -0700

    change number of test paths

--- cmpnet_test.sh
+++ cmpnet_test.sh
@@ -8,7 +8,7 @@ python3 cmpnet_test.py --model_path ../CMPnet_res/s2d/ \
 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 2 --data_path ../data/simple/ \
 --start_epoch 1 --memory_type res --env_type s2d --world_size 20 \
---total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2\
+--total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 \
 --seen_N 100 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
 --unseen_N 10 --unseen_NP 200 --unseen_s 100 --unseen_sp 0
 # seen: 100, 200, 0, 4000
