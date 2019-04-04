ac5d2f8347a8794be2eceee014783dd88fd07ff4
MiaoDragon <innocentmyl@gmail.com>
2019-04-04 11:41:29 -0700

    disable normalize

--- utility_r2d.py
+++ utility_r2d.py
@@ -4,7 +4,7 @@ import copy
 import numpy as np
 def normalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
-    #return x
+    return x
     bound = torch.tensor(bound)
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
@@ -12,25 +12,25 @@ def normalize(x, bound):
         #x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0] * 20.
         #print('before normalize...')
         #print(x[:,-2*len(bound):])
-        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound * 20.
-        x[:,-len(bound):] = x[:,-len(bound):] / bound * 20.
+        x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
+        x[:,-len(bound):] = x[:,-len(bound):] / bound
         #print('after normalize...')
         #print(x[:, -2*len(bound):])
     else:
-        x = x / bound * 20.
+        x = x / bound
     return x
 
 def unnormalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
     # x only one dim
-    #return x
+    return x
     bound = torch.tensor(bound)
     if len(x) != len(bound):
         # then the proceding is obstacle
         # don't normalize obstacles
         #x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0] / 20.
-        x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] / 20. * bound
-        x[-len(bound):] = x[-len(bound):] / 20. * bound
+        x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
+        x[-len(bound):] = x[-len(bound):] * bound
     else:
-        x = x / 20. * bound
+        x = x * bound
     return x
