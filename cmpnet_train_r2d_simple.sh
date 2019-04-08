00a1b04153b050fde2a7176c59c109a41c647860
MiaoDragon <innocentmyl@gmail.com>
2019-04-08 16:32:11 -0700

    change number of training. increase

--- cmpnet_train_r2d_simple.sh
+++ cmpnet_train_r2d_simple.sh
@@ -1,8 +1,8 @@
 # state space has 3DOF
 # input of MLP is thus 3*2+28=34
 python3 cmpnet_train.py --model_path ../CMPnet_res/r2d_simple/ \
---no_env 10 --no_motion_paths 400 --grad_step 1 --learning_rate 0.01 \
+--no_env 10 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
---n_tasks 1 --device 0 --freq_rehersal 20 --batch_rehersal 40 \
+--n_tasks 1 --device 0 --freq_rehersal 40 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --world_size 20 --env_type r2d_simple \
 --memory_type res --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3
