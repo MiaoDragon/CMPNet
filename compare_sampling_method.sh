973bab969685fd143657ae855ee11e8912796a49
MiaoDragon <innocentmyl@gmail.com>
2019-04-15 16:40:57 -0700

    visual

--- compare_sampling_method.sh
+++ compare_sampling_method.sh
@@ -1,9 +1,9 @@
 # state space has 3DOF
 # input of MLP is thus 3*2+28=34
 python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/ \
---no_env 2 --no_motion_paths 10 --grad_step 1 --learning_rate 0.01 \
---num_epochs 1 --memory_strength 0.5 --n_memories 8 \
---n_tasks 1 --device 0 --test_frequency 15 \
+--no_env 2 --no_motion_paths 5 --grad_step 1 --learning_rate 0.01 \
+--num_epochs 1 --memory_strength 0.5 --n_memories 4 \
+--n_tasks 1 --device 0 --test_frequency 11 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
 --memory_type prio_loss --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 --train_path 1 \
 --seen_N 2 --seen_NP 10 --seen_s 0 --seen_sp 4000 \
