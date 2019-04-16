32f4e9c30657b4aa260d1d59f516d46b74755de4
MiaoDragon <innocentmyl@gmail.com>
2019-04-15 19:38:26 -0700

    train only on 10 env and 4000 paths

--- compare_sampling_method_reward.sh
+++ compare_sampling_method_reward.sh
@@ -1,7 +1,7 @@
 # state space has 3DOF
 # input of MLP is thus 3*2+28=34
 python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/reward/ \
---no_env 100 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
+--no_env 10 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 3 --test_frequency 1000 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
