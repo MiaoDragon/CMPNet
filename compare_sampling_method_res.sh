15d0aa608b4f746c3e9e38ad76bb9db209c8a095
MiaoDragon <innocentmyl@gmail.com>
2019-04-17 14:02:59 -0700

    try reducing the number of paths again

--- compare_sampling_method_res.sh
+++ compare_sampling_method_res.sh
@@ -1,14 +1,13 @@
 # state space has 3DOF
 # input of MLP is thus 3*2+28=34
-python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/loss/ \
+python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/res/ \
 --no_env 10 --no_motion_paths 800 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 500 \
---n_tasks 1 --device 2 --test_frequency 1000 \
+--n_tasks 1 --device 3 --test_frequency 1000 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
---memory_type prio_loss --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 --train_path 1 \
+--memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 --train_path 1 \
 --seen_N 10 --seen_NP 50 --seen_s 0 --seen_sp 4000 \
 --unseen_N 10 --unseen_NP 50 --unseen_s 100 --unseen_sp 0
-
 #python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/ \
 #--no_env 2 --no_motion_paths 5 --grad_step 1 --learning_rate 0.01 \
 #--num_epochs 1 --memory_strength 0.5 --n_memories 4 \
