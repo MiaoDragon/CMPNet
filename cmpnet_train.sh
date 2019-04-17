15afd95ebcb0b07dd349f410d8f9ac0218eedb97
arclabdl1 <you@example.com>
2019-04-17 14:47:10 -0700

    debugging training, need to print loss

--- cmpnet_train.sh
+++ cmpnet_train.sh
@@ -4,10 +4,10 @@
 #--n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
 #--start_epoch 0 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ --world_size 20 --env_type c2d \
 #--memory_type res
-python cmpnet_train.py --model_path CMPnet_res/baxter_test_small_m20000_ms50_b0_f0/ \
+python cmpnet_train.py --model_path CMPnet_res/baxter_test_small_10000_ms50_b100_f230/ \
 --no_env 10 --no_motion_paths 500 --grad_step 1 --learning_rate 0.01 \
---num_epochs 1 --memory_strength 0.2 --n_memories 10000 \
---n_tasks 1 --device 0 --freq_rehersal 0 --batch_rehersal 0 \
+--num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
+--n_tasks 1 --device 0 --freq_rehersal 230 --batch_rehersal 100 \
 --start_epoch 0 --data_path data/test/ --world_size 20 --env_type baxter \
 --memory_type res --total_input_size 16067 --AE_input_size 16053 --mlp_input_size 74 --output_size 7 \
---dl1 0 --docker 0
+--dl1 1 --docker 0
