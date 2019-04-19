de491a41adc0a8e11c047049292c807fa6f771f9
MiaoDragon <innocentmyl@gmail.com>
2019-04-19 13:09:18 -0700

    change device

--- compare_sampling_method_reward.sh
+++ compare_sampling_method_reward.sh
@@ -3,7 +3,7 @@
 python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/reward/ \
 --no_env 10 --no_motion_paths 800 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 500 \
---n_tasks 1 --device 3 --test_frequency 1000 \
+--n_tasks 1 --device 1 --test_frequency 1000 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
 --memory_type prio_reward --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 --train_path 1 \
 --seen_N 10 --seen_NP 50 --seen_s 0 --seen_sp 4000 \
