24569219e5c0316ca4c359b2bd7111516ad49b8a
MiaoDragon <innocentmyl@gmail.com>
2019-04-23 18:10:28 -0700

    make layer wider to see if it works

--- Model/model_c2d_simple.py
+++ Model/model_c2d_simple.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 from torch.nn.utils.rnn import pack_padded_sequence
 from torch.autograd import Variable
 
-
+"""
 # DMLP Model-Path Generator
 class MLP(nn.Module):
 	def __init__(self, input_size, output_size):
@@ -17,6 +17,24 @@ class MLP(nn.Module):
 		nn.Linear(128, 64),nn.PReLU(), nn.Dropout(),
 		nn.Linear(64, 32),nn.PReLU(),
 		nn.Linear(32, output_size))  # added tanh to make sure output is -1 ~ 1
+
+	def forward(self, x):
+		out = self.fc(x)
+		return out
+"""
+
+# DMLP Model-Path Generator
+class MLP(nn.Module):
+	def __init__(self, input_size, output_size):
+		super(MLP, self).__init__()
+		self.fc = nn.Sequential(
+		nn.Linear(input_size, 1024),nn.PReLU(),nn.Dropout(),
+		nn.Linear(1024, 768),nn.PReLU(),nn.Dropout(),
+		nn.Linear(768, 512),nn.PReLU(), nn.Dropout(),
+		nn.Linear(512, 512),nn.PReLU(), nn.Dropout(),
+		nn.Linear(512, 128),nn.PReLU(), nn.Dropout(),
+		nn.Linear(128, 64),nn.PReLU(),
+		nn.Linear(64, output_size))  # added tanh to make sure output is -1 ~ 1
 
 	def forward(self, x):
 		out = self.fc(x)
