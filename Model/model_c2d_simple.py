f5481e534e2123f501c6f39e8d10aceabaf7716c
MiaoDragon <innocentmyl@gmail.com>
2019-04-25 12:27:56 -0700

    sarani wider network

--- Model/model_c2d_simple.py
+++ Model/model_c2d_simple.py
@@ -29,12 +29,12 @@ class MLP(nn.Module):
 		super(MLP, self).__init__()
 		self.fc = nn.Sequential(
 		nn.Linear(input_size, 1024),nn.PReLU(),nn.Dropout(),
-		nn.Linear(1024, 768),nn.PReLU(),nn.Dropout(),
-		nn.Linear(768, 512),nn.PReLU(), nn.Dropout(),
-		nn.Linear(512, 512),nn.PReLU(), nn.Dropout(),
-		nn.Linear(512, 128),nn.PReLU(), nn.Dropout(),
-		nn.Linear(128, 64),nn.PReLU(),
-		nn.Linear(64, output_size))  # added tanh to make sure output is -1 ~ 1
+		nn.Linear(1024, 1024),nn.PReLU(),nn.Dropout(),
+		nn.Linear(1024, 768),nn.PReLU(), nn.Dropout(),
+		nn.Linear(768, 768),nn.PReLU(), nn.Dropout(),
+		nn.Linear(768, 256),nn.PReLU(), nn.Dropout(),
+		nn.Linear(256, 128),nn.PReLU(),
+		nn.Linear(128, output_size))  # added tanh to make sure output is -1 ~ 1
 
 	def forward(self, x):
 		out = self.fc(x)
