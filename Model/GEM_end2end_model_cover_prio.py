0ff817ac2fd46fd1f9e3c0d6a8c6a111ff8ad80a
MiaoDragon <innocentmyl@gmail.com>
2019-05-01 00:49:04 -0700

    try

--- Model/GEM_end2end_model_cover_prio.py
+++ Model/GEM_end2end_model_cover_prio.py
@@ -123,7 +123,7 @@ class End2EndMPNet(nn.Module):
             data = torch.stack(data).cuda()
             labels = torch.stack(labels).cuda()
             sim_num = list(self.sim_num[t,:self.mem_cnt[t]])
-            sim_num += list(torch.IntTensor(len(data)-self.mem_cnt[t]))
+            sim_num += list(torch.IntTensor((len(data)-self.mem_cnt[t])))
             sim_num = torch.stack(sim_num).cuda()
             for j in range(len(x)):
                 dist = torch.norm((data - x[j].data) / 20, dim=1)
