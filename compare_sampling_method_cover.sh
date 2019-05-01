e4cb79e249bcd59ce01c3d8129ca4a1a04f33315
MiaoDragon <innocentmyl@gmail.com>
2019-05-01 00:46:41 -0700

    aa

--- compare_sampling_method_cover.sh
+++ compare_sampling_method_cover.sh
@@ -6,8 +6,8 @@ python3 cmpnet_compare_sample.py --model_path ../CMPnet_res/s2d/sample_exp/cover
 --n_tasks 1 --device 1 --test_frequency 1000 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
 --memory_type prio_cover --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 --train_path 1 \
---seen_N 10 --seen_NP 50 --seen_s 0 --seen_sp 4000 \
---unseen_N 10 --unseen_NP 50 --unseen_s 100 --unseen_sp 0
+--seen_N 0 --seen_NP 0 --seen_s 0 --seen_sp 4000 \
+--unseen_N 0 --unseen_NP 0 --unseen_s 100 --unseen_sp 0
 
 
 #10 50, 10 50
