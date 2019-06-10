55028749299f779cb9ded4b376e6eec6f310bde3
MiaoDragon <innocentmyl@gmail.com>
2019-06-10 14:24:57 -0700

    use python3

--- cmpnet_train.sh
+++ cmpnet_train.sh
@@ -4,7 +4,7 @@
 #--n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
 #--start_epoch 0 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ --world_size 20 --env_type c2d \
 #--memory_type res
-python2 cmpnet_train.py --model_path CMPnet_res/baxter/ \
+python cmpnet_train.py --model_path CMPnet_res/baxter/ \
 --no_env 10 --no_motion_paths 500 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 10 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 0 --freq_rehersal 100 --batch_rehersal 100 \
