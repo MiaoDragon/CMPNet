ee94f3d8d032ad150adeaba5b9b4a4178fc4a3e4
MiaoDragon <innocentmyl@gmail.com>
2019-06-14 12:56:41 -0700

    decrease data length to ensure the running is fine

--- exp/cmpnet_test_c2d.sh
+++ exp/cmpnet_test_c2d.sh
@@ -5,8 +5,8 @@ python3 cmpnet_test.py --model_path ../CMPnet_res/c2d/ \
 --n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ \
 --start_epoch 1 --memory_type res --env_type c2d --world_size 20 \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2 \
---seen_N 100 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
---unseen_N 10 --unseen_NP 2000 --unseen_s 100 --unseen_sp 0
+--seen_N 10 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
+--unseen_N 10 --unseen_NP 200 --unseen_s 100 --unseen_sp 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
 cd exp
--- exp/cmpnet_train_s2d.sh
+++ exp/cmpnet_train_s2d.sh
@@ -1,8 +1,11 @@
 cd ..
 python3 cmpnet_train.py --model_path ../CMPnet_res/s2d/ \
---no_env 100 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
+--no_env 10 --no_motion_paths 400 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 3 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
 --memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
 cd exp
+
+
+#100x4000
