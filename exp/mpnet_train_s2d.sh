850f9b4fca85a2ca9706bd05c7dd91c14aa12c12
MiaoDragon <innocentmyl@gmail.com>
2019-07-17 12:01:36 -0700

    size issue

--- exp/mpnet_train_s2d.sh
+++ exp/mpnet_train_s2d.sh
@@ -5,5 +5,5 @@ python3 mpnet_train.py --model_path ../MPnet_res/s2d/ \
 --num_epochs 10 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
---memory_type res --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3
+--memory_type res --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
 cd exp
