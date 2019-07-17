27b24378f7f8489da0a18719467e8750a14e097a
MiaoDragon <innocentmyl@gmail.com>
2019-07-17 11:59:42 -0700

    edited device number

--- exp/mpnet_train_s2d.sh
+++ exp/mpnet_train_s2d.sh
@@ -3,7 +3,7 @@ cd ..
 python3 mpnet_train.py --model_path ../MPnet_res/s2d/ \
 --no_env 10 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 10 --memory_strength 0.5 --n_memories 10000 \
---n_tasks 1 --device 0 --freq_rehersal 100 --batch_rehersal 100 \
+--n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --world_size 20 --env_type s2d \
 --memory_type res --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3
 cd exp
