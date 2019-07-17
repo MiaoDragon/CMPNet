a469961929a7052d5b443c2545e59cbb3fbe3ddc
MiaoDragon <innocentmyl@gmail.com>
2019-07-17 12:04:15 -0700

    data path error

--- exp/mpnet_train_s2d.sh
+++ exp/mpnet_train_s2d.sh
@@ -1,6 +1,6 @@
 cd ..
 # End-2-End learning (randomly shuffle path)
-python3 mpnet_train.py --model_path ../MPnet_res/s2d/ \
+python3 mpnet_train.py --model_path ../CMPnet_res/s2d/ \
 --no_env 10 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --num_epochs 10 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
