d52d6b97a2960625e992b1b8665b9773af5f8fcc
MiaoDragon <innocentmyl@gmail.com>
2019-07-18 11:28:55 -0700

    change model path to use the hard drive

--- exp/cmpnet_train_c2d.sh
+++ exp/cmpnet_train_c2d.sh
@@ -1,5 +1,5 @@
 1cd ..
-python3 cmpnet_train.py --model_path ../CMPnet_res/c2d/ \
+python3 cmpnet_train.py --model_path /media/arclabdl1/HD1/YLmiao/CMPnet_res/c2d/ \
 --no_env 100 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.001 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
