34ee523072f2d31a8558d7ab090406941d3c80b0
MiaoDragon <innocentmyl@gmail.com>
2019-11-07 14:59:22 -0800

    small size data first to test

--- exp/mpnet_test_home.sh
+++ exp/mpnet_test_home.sh
@@ -5,7 +5,7 @@ python3 cmpnet_test.py --model_path ../MPnet_res/home/ \
 --n_tasks 1 --device 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
 --start_epoch 10 --memory_type res --env_type home --world_size 20 \
 --total_input_size 150014 --AE_input_size 150000 --mlp_input_size 142 --output_size 7 \
---seen_N 1 --seen_NP 300 --seen_s 0 --seen_sp 4000 \
+--seen_N 1 --seen_NP 5 --seen_s 0 --seen_sp 4000 \
 --unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
