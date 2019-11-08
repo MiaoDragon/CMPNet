e3775147d27928bc479b57e29219f1bc813f767f
MiaoDragon <innocentmyl@gmail.com>
2019-11-07 16:03:38 -0800

    path index wrong

--- exp/cmpnet_test_home.sh
+++ exp/cmpnet_test_home.sh
@@ -5,7 +5,7 @@ python cmpnet_test.py --model_path ../CMPnet_res/home/ \
 --n_tasks 1 --device 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
 --start_epoch 1 --memory_type res --env_type home --world_size 20 \
 --total_input_size 150008 --AE_input_size 149994 --mlp_input_size 142 --output_size 7 \
---seen_N 1 --seen_NP 5 --seen_s 0 --seen_sp 4000 \
+--seen_N 1 --seen_NP 5 --seen_s 0 --seen_sp 2700 \
 --unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
