0fa8f4c8209bd8dc1ae1f843a3a0afc746ec1b54
MiaoDragon <innocentmyl@gmail.com>
2019-11-08 11:33:49 -0800

    use model path from harddrive

--- exp/cmpnet_train_home.sh
+++ exp/cmpnet_train_home.sh
@@ -1,6 +1,6 @@
 cd ..
 # End-2-End learning (randomly shuffle path)
-python cmpnet_train.py --model_path ../CMPnet_res/home/ \
+python cmpnet_train.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_res/home/ \
 --no_env 1 --no_motion_paths 2700 --grad_step 1 --learning_rate 0.0005 \
 --num_epochs 10 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 0 --freq_rehersal 100 --batch_rehersal 100 \
