30dc0bc6ac998faaa3521296a37ed5a1f48888ba
MiaoDragon <innocentmyl@gmail.com>
2019-11-08 12:31:59 -0800

    try using 4 epoch model

--- exp/cmpnet_test_home.sh
+++ exp/cmpnet_test_home.sh
@@ -3,7 +3,7 @@ python cmpnet_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_re
 --grad_step 1 --learning_rate 0.001 \
 --memory_strength 0.5 --n_memories 1 \
 --n_tasks 1 --device 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
---start_epoch 1 --memory_type res --env_type home --world_size 20 \
+--start_epoch 4 --memory_type res --env_type home --world_size 20 \
 --total_input_size 150008 --AE_input_size 149994 --mlp_input_size 142 --output_size 7 \
 --seen_N 1 --seen_NP 10 --seen_s 0 --seen_sp 2700 \
 --unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0
