0495bc7109a0d803d74f04ef8d5cd4ff9a378d5e
MiaoDragon <innocentmyl@gmail.com>
2019-11-13 11:49:05 -0800

    the channel position should be at the start

--- exp/cmpnet_test_home.sh
+++ exp/cmpnet_test_home.sh
@@ -4,7 +4,7 @@ python cmpnet_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_re
 --memory_strength 0.5 --n_memories 1 \
 --n_tasks 1 --device 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
 --start_epoch 4 --memory_type res --env_type home --world_size 20 \
---total_input_size 150008 --AE_input_size 32 32 32 1 --mlp_input_size 142 --output_size 7 \
+--total_input_size 150008 --AE_input_size 1 32 32 32 --mlp_input_size 142 --output_size 7 \
 --seen_N 1 --seen_NP 100 --seen_s 0 --seen_sp 2700 \
 --unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0
 # seen: 100, 200, 0, 4000
