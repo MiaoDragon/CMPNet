1c10ce3312df35dac07eddbd630bb0a93754e41b
MiaoDragon <innocentmyl@gmail.com>
2019-11-18 18:24:03 -0800

    num_epoch

--- exp/cmpnet_train_home_MLP3.sh
+++ exp/cmpnet_train_home_MLP3.sh
@@ -2,7 +2,7 @@ cd ..
 # End-2-End learning (randomly shuffle path)
 python cmpnet_train.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_res/home_mlp3/ \
 --no_env 1 --no_motion_paths 1800 --grad_step 1 --learning_rate 0.001 \
---num_epochs 20 --memory_strength 0.5 --n_memories 5000 \
+--num_epochs 100 --memory_strength 0.5 --n_memories 5000 \
 --n_tasks 1 --device 2 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ --world_size 20 --env_type home_mlp3 \
 --memory_type res --total_input_size 150008 --AE_input_size 1 32 32 32 --mlp_input_size 78 --output_size 7
