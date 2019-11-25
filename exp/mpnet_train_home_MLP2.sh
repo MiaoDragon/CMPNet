d559fbb6c788a8394878d2100682feb42bea29b9
MiaoDragon <innocentmyl@gmail.com>
2019-11-25 13:47:44 -0800

    smaller learning rate so we can guarantee more optimal

--- exp/mpnet_train_home_MLP2.sh
+++ exp/mpnet_train_home_MLP2.sh
@@ -1,7 +1,7 @@
 cd ..
 # End-2-End learning (randomly shuffle path)
 python mpnet_train.py --model_path /media/arclabdl1/HD1/YLmiao/results/MPnet_res/home_mlp2/ \
---no_env 1 --no_motion_paths 1800 --grad_step 1 --learning_rate 0.005 \
+--no_env 1 --no_motion_paths 1800 --grad_step 1 --learning_rate 0.001 \
 --num_epochs 500 --memory_strength 0.5 --n_memories 1 \
 --n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 --batch_size 256 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ --world_size 20 --env_type home_mlp2 \
