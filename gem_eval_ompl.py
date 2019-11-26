79541f5bba892284d89d659cca7c2568e86ba6df
MiaoDragon <innocentmyl@gmail.com>
2019-11-26 11:55:28 -0800

    what if we don't do lvc

--- gem_eval_ompl.py
+++ gem_eval_ompl.py
@@ -68,7 +68,7 @@ def eval_tasks(mpNet, test_data, filename, IsInCollision, normalize_func = lambd
                     path_vis = [p.numpy() for p in path]
                     path_vis = np.array(path_vis)
                     np.savetxt('path_%d_replan_%d.txt' % (j, t), path_vis, fmt='%f')
-                    path = lvc(path, obc[i], IsInCollision, step_sz=step_sz)
+                    #path = lvc(path, obc[i], IsInCollision, step_sz=step_sz)
                     #print('after lvc:')
                     #print(path)
                     if feasibility_check(path, obc[i], IsInCollision, step_sz=0.01):
