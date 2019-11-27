363a71c7e08d93b3171e417b86355f29176ebc4b
MiaoDragon <innocentmyl@gmail.com>
2019-11-26 22:09:52 -0800

    use 2196 paths

--- exp/cmpnet_train_home.sh
+++ exp/cmpnet_train_home.sh
@@ -1,7 +1,7 @@
 cd ..
 # End-2-End learning (randomly shuffle path)
 python cmpnet_train.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_res/home/ \
---no_env 1 --no_motion_paths 1800 --grad_step 1 --learning_rate 0.001 \
+--no_env 1 --no_motion_paths 2196 --grad_step 1 --learning_rate 0.001 \
 --num_epochs 100 --memory_strength 0.5 --n_memories 5000 \
 --n_tasks 1 --device 0 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ --world_size 20 --env_type home \
