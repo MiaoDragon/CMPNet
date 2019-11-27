f1542fecd21aef2c6b3e2bf4a151d9bbc9d8f310
MiaoDragon <innocentmyl@gmail.com>
2019-11-26 17:53:47 -0800

    try adding dist reorder

--- plan_general_ompl.py
+++ plan_general_ompl.py
@@ -171,7 +171,7 @@ def neural_replan(mpNet, path, obc, obs, IsInCollision, normalize, unnormalize, 
     if init_plan_flag:
         # if it is the initial plan, then we just do neural_replan
         #MAX_LENGTH = 80
-        MAX_LENGTH = 2400
+        MAX_LENGTH = 5000
         mini_path, time_d = neural_replanner(mpNet, path[0], path[-1], obc, obs, IsInCollision, \
                                             normalize, unnormalize, MAX_LENGTH, step_sz=step_sz)
         if mini_path:
@@ -188,7 +188,7 @@ def neural_replan(mpNet, path, obc, obs, IsInCollision, normalize, unnormalize, 
             else:
                 return path
     #MAX_LENGTH = 50
-    MAX_LENGTH = 5000
+    MAX_LENGTH = 8000
     # replan segments of paths
     new_path = [path[0]]
     time_norm = 0.
