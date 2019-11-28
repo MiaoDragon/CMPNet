bd8f4f34ce0683ac7faa9d9ba45fa51edfbf959f
MiaoDragon <innocentmyl@gmail.com>
2019-11-27 23:54:44 -0800

    test cmpnet_MLP2_lr01_SGD with 10 epoch

--- exp/cmpnet_test_home_MLP2_lr01_SGD.sh
+++ exp/cmpnet_test_home_MLP2_lr01_SGD.sh
@@ -6,7 +6,7 @@ python cmpnet_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/CMPnet_re
 --start_epoch 10 --memory_type res --env_type home_mlp2 --world_size 20 \
 --total_input_size 150008 --AE_input_size 1 32 32 32 --mlp_input_size 78 --output_size 7 \
 --seen_N 1 --seen_NP 500 --seen_s 0 --seen_sp 2196 \
---unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0
+--unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0 --use_local_reorder 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
 cd exp
