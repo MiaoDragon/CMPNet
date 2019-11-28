542b73b685b12b6d3498b427d0f639e087c95b25
MiaoDragon <innocentmyl@gmail.com>
2019-11-27 19:31:10 -0800

    use the first 500 paths for challenge dataset

--- exp/mpnet_test_home_MLP2_lr01_SGD_challenge.sh
+++ exp/mpnet_test_home_MLP2_lr01_SGD_challenge.sh
@@ -5,7 +5,7 @@ python mpnet_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/MPnet_res/
 --n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/YLmiao/data/home/challenge_data/ \
 --start_epoch 500 --memory_type res --env_type home_mlp2 --world_size 20 \
 --total_input_size 150008 --AE_input_size 1 32 32 32 --mlp_input_size 78 --output_size 7 \
---seen_N 1 --seen_NP 500 --seen_s 0 --seen_sp 2196 \
+--seen_N 1 --seen_NP 500 --seen_s 0 --seen_sp 0 \
 --unseen_N 0 --unseen_NP 0 --unseen_s 0 --unseen_sp 0 --use_local_reorder 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
