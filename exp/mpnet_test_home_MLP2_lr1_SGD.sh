a9315869d0973867ec08d24b2e5cd2c082d76e16
MiaoDragon <innocentmyl@gmail.com>
2019-11-30 10:36:41 -0800

    device

--- exp/mpnet_test_home_MLP2_lr1_SGD.sh
+++ exp/mpnet_test_home_MLP2_lr1_SGD.sh
@@ -2,7 +2,7 @@ cd ..
 python mpnet_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/MPnet_res/home_mlp2_lr1_SGD/ \
 --grad_step 1 --learning_rate 0.001 \
 --memory_strength 0.5 --n_memories 1 \
---n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
+--n_tasks 1 --device 3 --data_path /media/arclabdl1/HD1/YLmiao/data/home/ \
 --start_epoch 500 --memory_type res --env_type home_mlp2 --world_size 20 \
 --total_input_size 150008 --AE_input_size 1 32 32 32 --mlp_input_size 78 --output_size 7 \
 --seen_N 1 --seen_NP 500 --seen_s 0 --seen_sp 2196 \
