a6c29028d4730f452c65edd0a928b0964aee7755
MiaoDragon <innocentmyl@gmail.com>
2019-12-06 20:50:29 -0800

    a

--- c++/test_sample_gen.py
+++ c++/test_sample_gen.py
@@ -1,4 +1,4 @@
 import numpy as np
 # generate random test sample for comparing cpp and py
-data = np.random.uniform(low=[-1.0]*14, high=[-1.0]*14)
+data = np.random.uniform(low=[-1.0]*14, high=[1.0]*14)
 np.savetxt('test_sample.txt', data, delimiter='\n', fmt='%f')
